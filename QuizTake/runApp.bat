707de22880abd553cfb138575c709c84e5122797
DevinPate1 <devinpatel02@gmail.com>
2023-09-23 00:24:56 -0500

    Set up run script

---
+++ QuizTake/runApp.bat
@@ -0,0 +1,19 @@
+@ECHO OFF
+
+set FLASK_APP=app:app
+set /A WORKERS=2
+
+Rem Find number of arguments
+set /A argC=0
+for %%x in (%*) do set /A argC+=1
+
+Rem Check if there are any arguments
+Rem Set default values if not
+if %argC%==0 (
+    BIND=127.0.0.1:8000
+) else (
+    set BIND=%1
+)
+
+echo "waitress-serve --threads=%WORKERS% --listen=%BIND% %FLASK_APP%"
+waitress-serve --threads=%WORKERS% --listen=%BIND% %FLASK_APP%
