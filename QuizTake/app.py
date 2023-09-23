646f168833eb780d81bed923d3d6259d7c3b874f
DevinPate1 <devinpatel02@gmail.com>
2023-09-22 22:52:49 -0500

    Init QuizTake branch

---
+++ QuizTake/app.py
@@ -0,0 +1,28 @@
+###################################################################
+# Quarzlet Take Flask Server
+# Group 3: Devin Patel, Vish Patel, Aidan Lear
+# Purpose: To implement a "Take Quiz" service for the Quarzlet application
+#
+# The service must:
+#   - Provide an entry point for the Quarzlet application.
+#   - The first page must prompt the user to either take a quiz or redirect to the QuizMake service.
+#   - Send a request to the QuizStore service to fetch all quiz names.
+#   - Present the user with a list of quiz names to select from as a sidebar item.
+#   - Query the user to select a quiz to take.
+#   - Send a request to the QuizStore service to fetch the selected quiz.
+#   - Present the quiz to the user and allow them to select one of multiple choice answers.
+#   - Grade the quiz according to the correct answers
+#   - Present the user with their score
+#   - Allow the user to reset the quiz to take it again.
+#   - The state of the quiz (i.e., the selected answers or graded quiz) should be saved even if the user clicks to a different quiz.
+#     - The state of the quiz can get reset if the user refreshes the page or leaves the site.
+###################################################################
+
+# Imports
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
