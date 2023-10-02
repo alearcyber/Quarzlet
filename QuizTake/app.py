6856ec4e437f22018e7e4889165a15b22d94bc87
DevinPatel1 <devin.patel72@gmail.com>
2023-10-01 20:01:20 -0500

    Finished Quiz Design

--- QuizTake/app.py
+++ QuizTake/app.py
@@ -12,22 +12,22 @@
 #   - Send a request to the QuizStore service to fetch the selected quiz.
 #   - Present the quiz to the user and allow them to select one of multiple choice answers.
 #   - Grade the quiz according to the correct answers
-#   - Present the user with their score
-#   - Allow the user to reset the quiz to take it again.
-#   - The state of the quiz (i.e., the selected answers or graded quiz) should be saved even if the user clicks to a different quiz.
-#     - The state of the quiz can get reset if the user refreshes the page or leaves the site.
+#   - Present the user with their score.
 ###################################################################
 
 # Imports
 from flask import Flask, render_template, redirect, url_for, request
+import random
 
 app = Flask(__name__)
 
 
 def generate_quiz_list():
     # Make database query for all quiz titles
-    quiz_titles = ["Pikachu"*10, "Charizard", "Squirtle", "Jigglypuff",
+    quiz_titles = ["Pikachu"*20, "Charizard", "Squirtle", "Jigglypuff",
                    "Bulbasaur", "Gengar", "Charmander", "Mew", "Lugia", "Gyarados"]
+    quiz_titles += quiz_titles
+    quiz_titles += quiz_titles
     quiz_urls = []
 
     for title in quiz_titles:
@@ -51,16 +51,27 @@ def quiz():
     quiz_name = request.args.get("quiz")
     
     # Use the name to fetch the quiz data from the database
+    # @TODO Randomize the order of the questions and answers
+    quiz_data = [{"question_num": 1, "question": "What is the best pokemon?", "answers": ["Pikachu", "Charizard", "Squirtle", "Jigglypuff"]},
+                 {"question_num": 2, "question": "What is 2 + 2?", "answers": ["4", "5", "6", "7"]},
+                 {"question_num": 3, "question": "Who invented the telephone?", "answers": ["Alexander Graham Bell", "Thomas Edison", "Nikola Tesla", "Albert Einstein"]},
+                 {"question_num": 4, "question": "What color does red and blue make?", "answers": ["Purple", "Green", "Orange", "Yellow"]}]
+    
     # If it is not there, then redirect to quiz not found page.
-    quiz_data = {}
-    
-    return quiz_not_found()
-    
+    if not quiz_data:
+        return quiz_not_found()
     
     # Update current quiz name in session and render webpage
     app.jinja_env.globals.update(current_quiz=quiz_name)
-    return render_template("quiz.html")
     return render_template("quiz.html", quiz_data=quiz_data)
+
+@app.route("/submit", methods=["POST"])
+def check_quiz():
+    if request.method != 'POST':
+        return render_template("quiz_not_found.html")
+    
+    # 
+        
 
 
 def quiz_not_found():
