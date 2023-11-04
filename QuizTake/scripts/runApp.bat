2970548c4996c3e5549f26f65fb100e8501b357c
DevinPate1 <devinpatel02@gmail.com>
2023-11-04 11:07:32 -0500

    Created dockerfile

--- QuizTake/scripts/runApp.bat
+++ QuizTake/scripts/runApp.bat
@@ -3,6 +3,8 @@
 cd ..
 
 set FLASK_APP=app:app
+set HOST=127.0.0.1
+set PORT=8000
 set /A WORKERS=2
 
 Rem Find number of arguments
@@ -12,7 +14,7 @@ for %%x in (%*) do set /A argC+=1
 Rem Check if there are any arguments
 Rem Set default values if not
 if %argC%==0 (
-    set BIND=127.0.0.1:8000
+    set BIND=%HOST%:%PORT%
 ) else (
     set BIND=%1
 )
--- QuizTake/scripts/runApp.sh
+++ QuizTake/scripts/runApp.sh
@@ -5,12 +5,14 @@ SCRIPT_DIR=$(pwd)
 cd ..
 
 FLASK_APP="app:app"
+HOST=127.0.0.1
+PORT=8000
 WORKERS=2
 
 # Check if there are any arguments
 # Set default values if not
 if [[ $ARGC==0 ]] then
-    BIND=127.0.0.1:8000
+    BIND=$HOST:$PORT
 else
     BIND=$1
 fi
