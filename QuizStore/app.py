ca5851436d588016a1904df1a08bf77c152f8923
DevinPate1 <devinpatel02@gmail.com>
2023-11-04 14:11:27 -0500

    Added Ports to Dockerfile

--- QuizStore/app.py
+++ QuizStore/app.py
@@ -8,14 +8,10 @@ app = Flask(__name__, root_path='./QuizStore/')
 DB_PATH = "db"
 
 
-
-
-
-
 @app.route('/')
 def testpage():
-    #out = available_quizzes()
-    #return jsonify(out)
+    # out = available_quizzes()
+    # return jsonify(out)
     return "hello"
 
 ################################################################################################
@@ -28,9 +24,11 @@ def testpage():
 #    460, poorly formatted request
 #    461, Can't find existing quiz of that name
 ################################################
+
+
 @app.route('/getquiz', methods=['GET'])
 def getquiz():
-    #parse the incoming JSON
+    # parse the incoming JSON
     content: dict = request.json
 
     # check request formatting
@@ -46,7 +44,6 @@ def getquiz():
     return jsonify(quiz)
 
 
-
 # Inserts a new quiz to the database
 @app.route('/addquiz', methods=['POST'])
 def addquiz():
@@ -56,8 +53,7 @@ def addquiz():
     except:
         return "ERROR: JSONParseError - The JSON could not be found, or it could not be parsed as JSON", 462
 
-
-    #check to see if expected keys are present
+    # check to see if expected keys are present
     keys = content.keys()
     if "name" not in keys:
         return f"ERROR: PoorlyFormattedRequest - \"name\" key cannot be found.", 460
@@ -66,22 +62,23 @@ def addquiz():
     if "quiz" not in keys:
         return f"ERROR: PoorlyFormattedRequest - \"quiz\" key cannot be found.", 460
 
-    #now attempts to parse the rest of the information from the quiz
+    # now attempts to parse the rest of the information from the quiz
     try:
         question_list = content['quiz']
         for q in question_list:
-            valid = ("question" in q.keys()) and ("correctanswer" in q.keys()) and ("badanswers" in q.keys())
+            valid = ("question" in q.keys()) and (
+                "correctanswer" in q.keys()) and ("badanswers" in q.keys())
             if not valid:
                 return f"ERROR: PoorlyFormattedRequest - One of the questions is missing a key in the quiz.", 460
     except:
         return f"ERROR: PoorlyFormattedRequest - There is an unknown issue with the formatting of the sent quiz.", 460
 
-
-    #add the quiz
+    # add the quiz
     try:
         add_new_quiz(content)
     except:
-        jsonify({"status": 0, "Message": "There was an issue adding the quiz to the database. It was NOT added"})
+        jsonify(
+            {"status": 0, "Message": "There was an issue adding the quiz to the database. It was NOT added"})
 
     return jsonify({"status": 1, "Message": "Quiz Was Successfully Added"})
 
@@ -94,25 +91,20 @@ def available():
     return jsonify(available_quizzes())
 
 
-
-
-
-
-
 ################################################################################################
 # Helper Routines
 ################################################################################################
 
-#checks to see if the quiz name exists in the database
+# checks to see if the quiz name exists in the database
 def does_quiz_exist(name):
     results = query(f"""SELECT * FROM quiz where name=?""", (name,))
     return len(results) > 0
 
 
-#grab the quiz from the database with the given name
+# grab the quiz from the database with the given name
 # Returns it as a python dictionary
 def get_quiz(name):
-    #grab quiz
+    # grab quiz
     q = """
     SELECT
         quiz.name, quiz.description, question.question_text, question.right_answer, wrong.answer_text
@@ -126,9 +118,9 @@ def get_quiz(name):
 
     results = query(q, (name,))
 
-    #parse info describing the quiz
-    name = results[0][0] #name of the quiz
-    description = results[0][1] #description of quiz
+    # parse info describing the quiz
+    name = results[0][0]  # name of the quiz
+    description = results[0][1]  # description of quiz
     correct_answers = dict()
     incorrect_answers = dict()
     for _, _, question, correctanswer, badanswer in results:
@@ -137,23 +129,21 @@ def get_quiz(name):
             incorrect_answers[question] = []
         incorrect_answers[question].append(badanswer)
 
-    #create list of questions
+    # create list of questions
     questions = []
     for _, _, question, _, _ in results:
-        q = dict() #dictionary to represent a question in the list of questions
+        q = dict()  # dictionary to represent a question in the list of questions
         q["question"] = question
         q["correctanswer"] = correct_answers[question]
         q["badanswers"] = incorrect_answers[question]
         questions.append(q)
 
-    #construct final dictionary of the output
+    # construct final dictionary of the output
     quiz = dict()
     quiz['name'] = name
     quiz['description'] = description
     quiz['quiz'] = questions
     return quiz
-
-
 
 
 # adds a new quiz to the database
@@ -164,26 +154,23 @@ def add_new_quiz(data):
     description = data['description']
     questions = data['quiz']
 
-
-    #insert into quiz table first
+    # insert into quiz table first
     query(f"""INSERT INTO quiz VALUES (?, ?)""", (name, description))
 
-
-    #Add all the questions
+    # Add all the questions
     for question in questions:
-        #data
+        # data
         q = question['question']
         a = question['correctanswer']
         wrong_answers = question['badanswers']
 
-        #insert into database
+        # insert into database
         query(f"""INSERT INTO question (question_text, right_answer, quiz_name) VALUES (?, ?, ?)""", (q, a, name))
 
-        #add bad answers
+        # add bad answers
         for wrong_answer in wrong_answers:
-            query(f"""INSERT INTO wrong (question_text, answer_text) VALUES (?, ?)""", (q, wrong_answer))
-
-
+            query(
+                f"""INSERT INTO wrong (question_text, answer_text) VALUES (?, ?)""", (q, wrong_answer))
 
 
 ##########################################################################################
@@ -226,8 +213,5 @@ def main():
     app.run(host='0.0.0.0', port=port)
 
 
-
-
 if __name__ == '__main__':
     main()
-
--- QuizTake/app.py
+++ QuizTake/app.py
@@ -19,15 +19,18 @@
 from flask import Flask, render_template, redirect, url_for, request, session
 import random
 import configparser
+import requests
+
 
 def init_flask_config():
     # Initialize the Flask secret key from the config file and return it
-    
+
     # Check if config file exists
     import os
     if not os.path.exists(r'config/flask.ini'):
-        raise Exception("Please create a config/flask.ini file before running the server. See config/flask.ini.example for an example.")
-    
+        raise Exception(
+            "Please create a config/flask.ini file before running the server. See config/flask.ini.example for an example.")
+
     # Read the secret key from the config file
     config = configparser.ConfigParser()
     config.read(r'config/flask.ini')
@@ -35,9 +38,10 @@ def init_flask_config():
     if len(config['credentials']['secret']) < 64:
         raise Exception("Please change the secret key to a cryptographically secure token in config/flask.ini before running the server.\n"
                         + "The token must be at least 64 characters. One can be generated in the Python REPL with secrets.token_hex(32).")
-    
+
     return config['credentials']['secret']
-    
+
+
 ###################################################################
 # Main - Initialize the Flask app
 ###################################################################
@@ -48,30 +52,29 @@ app.secret_key = init_flask_config()
 # App Functions
 def generate_quiz_list():
     # Create quiz list for sidebar
-    # @TODO Make database query for all quiz titles
-    # @DEBUG - Hardcoded quiz data
-    quiz_titles = ["Pikachu"*20, "Charizard", "Squirtle", "Jigglypuff",
-                   "Bulbasaur", "Gengar", "Charmander", "Mew", "Lugia", "Gyarados"]
-    quiz_titles += quiz_titles
-    quiz_titles += quiz_titles
+    response = requests.get("http://quarzlet-store-cont/available")
+    data = response.json()
+    print(data)
+    quiz_titles = ["Pokemon", "Math", "History", "Colors"]
     quiz_urls = []
 
     # Assign a url for every quiz title
     for title in quiz_titles:
         quiz_urls.append({"name": title, "url": url_for("quiz", quiz=title)})
     quizzes_len = len(quiz_urls)
-    
+
     # Set the user's current quiz to empty
     session['current_quiz'] = ""
-    
+
     # Update the global variables for the quiz list and the current quiz. quizzes_len is used by the Jinja syntax to determine if the quiz list is empty or not.
-    app.jinja_env.globals.update(quizzes=quiz_urls, quizzes_len=quizzes_len, current_quiz=session['current_quiz'])
+    app.jinja_env.globals.update(
+        quizzes=quiz_urls, quizzes_len=quizzes_len, current_quiz=session['current_quiz'])
 
 
 @app.route("/")
 def index():
     # Landing page for the app
-    
+
     # Quiz list for sidebar
     generate_quiz_list()
 
@@ -82,19 +85,21 @@ def index():
 def quiz():
     # Get the quiz name
     quiz_name = request.args.get("quiz")
-    
+
     # Use the name to fetch the quiz data from the database
     # @TODO Randomize the order of the questions and answers
     # @DEBUG - Hardcoded quiz data
     quiz_data = [{"question_num": 0, "question": "What is the best pokemon?", "answers": ["Pikachu", "Charizard", "Squirtle", "Jigglypuff"], "correct_answer": "Pikachu"},
-                 {"question_num": 1, "question": "What is 2 + 2?", "answers": ["4", "5", "6", "7"], "correct_answer": "4"},
-                 {"question_num": 2, "question": "Who invented the telephone?", "answers": ["Alexander Graham Bell", "Thomas Edison", "Nikola Tesla", "Albert Einstein"], "correct_answer": "Alexander Graham Bell"},
+                 {"question_num": 1, "question": "What is 2 + 2?",
+                     "answers": ["4", "5", "6", "7"], "correct_answer": "4"},
+                 {"question_num": 2, "question": "Who invented the telephone?", "answers": [
+                     "Alexander Graham Bell", "Thomas Edison", "Nikola Tesla", "Albert Einstein"], "correct_answer": "Alexander Graham Bell"},
                  {"question_num": 3, "question": "What color does red and blue make?", "answers": ["Purple", "Green", "Orange", "Yellow"], "correct_answer": "Purple"}]
-    
+
     # If it is not there, then redirect to quiz not found page.
     if not quiz_data:
         redirect(url_for("quiz_not_found"))
-    
+
     # Update current quiz name in session and render webpage
     session['quiz_data'] = quiz_data
     session['current_quiz'] = quiz_name
@@ -106,25 +111,27 @@ def quiz():
 def check_quiz():
     if request.method != 'POST':
         return render_template("quiz_not_found.html")
-    
+
     user_answers = []
     num_correct = 0
-    
+
     # Fetch quiz data from session
     quiz_data = session['quiz_data']
-    
+
     # Check user answers against correct answers
     for question in quiz_data:
         question_num = question["question_num"]
         user_answer = request.form.get("question{}".format(question_num))
         user_answers.append(user_answer)
-        if user_answer == question["correct_answer"]: num_correct += 1
-        
+        if user_answer == question["correct_answer"]:
+            num_correct += 1
+
     # Pack data for results page
-    results = {"user_answers": user_answers, "quiz_data": quiz_data, "num_correct": num_correct, "num_questions": len(quiz_data)}
-    
+    results = {"user_answers": user_answers, "quiz_data": quiz_data,
+               "num_correct": num_correct, "num_questions": len(quiz_data)}
+
     return render_template("results.html", results=results)
-    
+
 
 @app.route("/quiz_not_found")
 def quiz_not_found():
@@ -132,4 +139,3 @@ def quiz_not_found():
     session['current_quiz'] = ""
     app.jinja_env.globals.update(current_quiz=session['current_quiz'])
     return render_template("quiz_not_found.html")
-
