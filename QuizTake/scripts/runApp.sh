ac376c61453ee60c7c8e4eb49832761dc1fa37e3
DevinPate1 <devinpatel02@gmail.com>
2023-11-04 12:37:54 -0500

    Fixed dockerfile and run shell

--- QuizTake/scripts/runApp.sh
+++ QuizTake/scripts/runApp.sh
@@ -2,20 +2,22 @@
 
 
 SCRIPT_DIR=$(pwd)
-cd ..
+cd /app
+echo "Running from $(pwd)"
 
 FLASK_APP="app:app"
-HOST=127.0.0.1
+HOST=0.0.0.0
 PORT=8000
+BIND=$HOST:$PORT
 WORKERS=2
 
 # Check if there are any arguments
 # Set default values if not
-if [[ $ARGC==0 ]] then
-    BIND=$HOST:$PORT
-else
-    BIND=$1
-fi
+# if [[ $ARGC==0 ]] then
+#     BIND=$HOST:$PORT
+# else
+#     BIND=$1
+# fi
 
 echo "gunicorn -w $WORKERS -b $BIND $FLASK_APP"
 gunicorn -w $WORKERS -b $BIND $FLASK_APP
