17ab322698d189985398237eecc3113f2ad59a38
DevinPatel1 <devin.patel72@gmail.com>
2023-11-05 16:03:01 -0600

    Undid docker ignore change

--- QuizTake/scripts/runApp.sh
+++ QuizTake/scripts/runApp.sh
@@ -12,11 +12,11 @@ WORKERS=2
 
 # Check if there are any arguments
 # Set default values if not
-# if [[ $ARGC==0 ]] then
-#     BIND=$HOST:$PORT
-# else
-#     BIND=$1
-# fi
+if [[ $ARGC==0 ]] then
+    BIND=$HOST:$PORT
+else
+    BIND=$1
+fi
 
 echo "gunicorn -w $WORKERS -b $BIND $FLASK_APP"
 gunicorn -w $WORKERS -b $BIND $FLASK_APP
